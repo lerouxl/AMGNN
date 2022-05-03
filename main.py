57dda4823a244cb4fe2535435d311bf071c3b6e8
Léopold Le Roux <leopold.le_roux@protonmail.com>
2022-05-03 17:05:09 +0100

    Use Pytorch lightning to deal with the model

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
+import torch
 import wandb
 from utils.config import read_config
 from pathlib import Path
-from model.amgnn import model
-
+from model.amgnn import AMGNNmodel
+from dataloader.arc_dataset import ARCDataset
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from torch_geometric.loader import DataLoader
+from utils.visualise import display_X_Y_Y_hat_data
 
 def run():
     """Train an test AMGNN model on the data"""
     # Initialise wandb
     configuration = read_config(Path("configs"))
+
+    wandb_logger = WandbLogger(project="AMGNN", log_model=True, mode="offline", config=configuration)
     wandb.init(mode="offline", config=configuration)
 
     # Access all hyperparameters values through wandb.config
     configuration = wandb.config
     print(configuration)
 
-    amgnn = model()
-    amgnn.load_data()
-    amgnn.build()
-    amgnn.train()
-    amgnn.validate()
+    model = AMGNNmodel(configuration)
+    DATA_PATH = Path("data/light")
+    arc_dataset = ARCDataset(DATA_PATH)
+    arc_loader = DataLoader(dataset=arc_dataset , batch_size= 2, shuffle= True)
+
+    trainer = pl.Trainer(limit_train_batches=10, max_epochs=1, accelerator="gpu", devices=1, logger=wandb_logger)
+    trainer.fit(model=model, train_dataloaders=arc_loader)
+
+    with torch.no_grad():
+        model.eval()
+        batch = arc_dataset[0]
+        y_hat = model.network(batch)
+        display_X_Y_Y_hat_data(pos=batch.pos.numpy(), X=batch.x.numpy(),
+                               Y=batch.y.numpy(), Y_hat=y_hat.numpy(), config=configuration)
 
 
 if __name__ == "__main__":
--- model/amgnn.py
+++ model/amgnn.py
@@ -1,44 +1,97 @@
-import torch.nn as nn
+from torch import optim, nn
+from typing import Tuple
+import torch
 import wandb
-from model.GNCConv import GCNConv
+#from model.GNCConv import GCNConv
+from torch_geometric.nn import MessagePassing, GCNConv
 from dataloader.arc_dataset import ARCDataset
+import pytorch_lightning as pl
 
 
-class model():
-    """Main class of AMGNN. This class will deal with the model loading, training, testing, validation and logings"""
+class AMGNNmodel(pl.LightningModule):
+    """Main class of AMGNN. This Pytorch lightning class will deal with the model loading, training, testing, validation
+     and logings"""
 
-    def __init__(self):
-        self.configuration = wandb.config
-
-    def build(self):
-        """Create the neural network using wandb parameters"""
-        self.network = NeuralNetwork(self.configuration.hidden_channels, self.configuration.out_channels)
-
-
-    def train(self):
-        """Train the neural network"""
-        pass
-
-    def validate(self):
-        """Test the neural network on a test or validation dataset"""
-        pass
-
-    def predict(self):
-        """Apply the neural network to make prediction """
-        pass
-
-    def load_data(self):
-        """Load and preprocess the dataset"""
-        self.dataset = ARCDataset(self.configuration.raw_data, )
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self, hidden_channels, out_channels):
+    def __init__(self, config):
         super().__init__()
-        self.conv1 = GCNConv(hidden_channels, out_channels)
-        self.conv2 = GCNConv(out_channels, out_channels)
+        self.configuration = config
+        self.network = NeuralNetwork(self.configuration.input_channels, self.configuration.hidden_channels,
+                                     self.configuration.out_channels)
+        self.Floss = nn.functional.mse_loss
+        self.lr_val = self.configuration.learning_rate
+        # self.example_input_array = torch.Tensor(32, 1, 28, 28)
+        #self.save_hyperparameters()
 
-    def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        x = self.conv2(x, edge_index)
+
+    def training_step(self, batch, batch_idx):
+        """Train loop of the neural network"""
+        _, loss, loss_disp, loss_temp = self._get_preds_loss(batch)
+        self.log("train loss", loss)
+        self.log("train displacement loss", loss_disp)
+        self.log("train temperature loss", loss_temp)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        """The test set is NOT used during training, it is ONLY used once the model has been trained to see how the
+         model will do in the real-world."""
+        _, loss, loss_disp, loss_temp = self._get_preds_loss(batch)
+        self.log("test loss", loss)
+        self.log("test displacement loss", loss_disp)
+        self.log("test temperature loss", loss_temp)
+
+    def validation_step(self, batch, batch_idx):
+        """During training, it’s common practice to use a small portion of the train split to determine when the model
+         has finished training.As a rule of thumb, we use 20% of the training set as the validation set.
+         This number varies from dataset to dataset."""
+        _, loss, loss_disp, loss_temp = self._get_preds_loss(batch)
+        self.log("val loss", loss)
+        self.log("val displacement loss", loss_disp)
+        self.log("val temperature loss", loss_temp)
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.lr_val)
+        return optimizer
+
+    def _get_preds_loss(self, batch) -> Tuple[torch.Tensor, torch.Tensor]:
+        """The train, test and validation function are the same so their are regrouped here.
+        Return:
+            y_hat: the prediction of the neural network,
+            loss: the actual loss of the neural network"""
+        y_hat = self.network(batch)
+        temp_hat = y_hat[:,0]
+        temp = batch.y[:,0]
+
+        disp_hat = y_hat[:,1:-1]
+        disp = batch.y[:,1:-1]
+        loss_temp = self.Floss(temp_hat, temp)
+        loss_disp = self.Floss(disp_hat, disp)
+        loss = loss_disp + loss_temp #self.Floss(y_hat, batch.y)
+        return y_hat, loss, loss_disp, loss_temp
+
+
+class NeuralNetwork(MessagePassing):
+    def __init__(self,in_channels,  hidden_channels, out_channels):
+        super().__init__(aggr='add')
+
+        self.lin1 = nn.Linear(in_channels, hidden_channels)
+        self.lin2 = nn.Linear(hidden_channels, out_channels)
+        self.conv1 = GCNConv(hidden_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.act = nn.LeakyReLU()
+
+    def forward(self, batch):
+        x = batch.x
+        edge_index = batch.edge_index
+        edge_attr = batch.edge_attr
+
+        x = self.lin1(x)
+        x = self.act(x)
+
+        x = self.conv1(x=x, edge_index=edge_index.long(), edge_weight=edge_attr)
+        x = self.act(x)
+
+        x = self.conv2(x=x, edge_index=edge_index.long(), edge_weight=edge_attr)
+        x = self.act(x)
+
+        x= self.lin2(x)
         return x
