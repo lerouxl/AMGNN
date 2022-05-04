4e53e12455ab4a5fed229bc07ee596c6b89b15d6
Léopold Le Roux <leopold.le_roux@protonmail.com>
2022-05-04 12:21:04 +0100

    Update visualisation tools to create a vtk file per simulation step, drop polyscope.

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from dataloader.arc_dataset import ARCDataset
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 from torch_geometric.loader import DataLoader
-from utils.visualise import display_X_Y_Y_hat_data
+from utils.visualise import display_dataset
+
 
 def run():
     """Train an test AMGNN model on the data"""
@@ -15,35 +16,42 @@ def run():
     configuration = read_config(Path("configs"))
 
     wandb_logger = WandbLogger(project="AMGNN", log_model=True, mode="offline", config=configuration)
-    wandb.init(mode="offline", config=configuration)
 
     # Access all hyperparameters values through wandb.config
     configuration = wandb.config
     print(configuration)
 
+    # Create the deep learning model
     model = AMGNNmodel(configuration)
-    DATA_PATH = Path("data/light") # TODO: Use the path from the configuration
-    arc_dataset = ARCDataset(DATA_PATH)
-    dataset_size = len(arc_dataset)
-    train_size = int(dataset_size * 0.80)
-    validation_size = int(dataset_size * 0.15)
-    test_size = dataset_size - train_size - validation_size
-    train_dataset, validation_dataset, test_dataset = torch.utils.data.random_split(arc_dataset,[train_size, validation_size, test_size], generator=torch.Generator().manual_seed(51))
 
-    train_loader = DataLoader(dataset=train_dataset , batch_size= 2, shuffle= True)
+    # Create the dataset
+    data_path = Path(configuration["raw_data"])
+    arc_dataset = ARCDataset(data_path)
+
+    # Split the dataset in 3 subset, the train, validation and test dataset.
+    dataset_size = len(arc_dataset)
+    train_size = int(dataset_size * 0.80)  # 80% of the dataset
+    validation_size = int(dataset_size * 0.15)  # 15% of the dataset
+    test_size = dataset_size - train_size - validation_size  # 5% of the dataset
+    # Split the dataset with a repeatable way (the random generator is fixed)
+    train_dataset, validation_dataset, test_dataset = torch.utils.data.random_split(arc_dataset,
+                                                                                    [train_size, validation_size,
+                                                                                     test_size],
+                                                                                    generator=torch.Generator().manual_seed(
+                                                                                        51))
+
+    # Create the dataloader
+    train_loader = DataLoader(dataset=train_dataset, batch_size=2, shuffle=True)
     validation_loader = DataLoader(dataset=validation_dataset, batch_size=2, shuffle=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=2, shuffle=True)
 
-    trainer = pl.Trainer(limit_train_batches=10, max_epochs=5, accelerator="gpu", devices=1, logger=wandb_logger)
+    # Create the pytorch lightning trainer.
+    trainer = pl.Trainer(accelerator="gpu", devices=1, logger=wandb_logger)
     trainer.fit(model, train_loader, validation_loader)
     trainer.test(dataloaders=test_loader)
 
-    with torch.no_grad():
-        model.eval()
-        batch = arc_dataset[0]
-        y_hat = model.network(batch)
-        display_X_Y_Y_hat_data(pos=batch.pos.numpy(), X=batch.x.numpy(),
-                               Y=batch.y.numpy(), Y_hat=y_hat.numpy(), config=configuration)
+    # Display the test dataset results in vtk files (can be open with Paraview)
+    display_dataset(model, test_dataset, configuration)
 
 
 if __name__ == "__main__":
--- utils/visualise.py
+++ utils/visualise.py
@@ -1,45 +1,96 @@
-import polyscope as ps
+import torch
+import pyvista as pv
+from pathlib import Path
 import numpy as np
+from model.amgnn import AMGNNmodel
+from torch.utils.data import Subset
 
 
-def display_X_Y_Y_hat_data(pos, X, Y, Y_hat=None, display_features=False, config=None):
+def display_dataset(model: AMGNNmodel, dataset: Subset, configuration):
+    """
+    Generate vtk representation of the dataset.
+    Note, no loss are calculated.
+    Note, all files in the dataloader folder are processed.
+    :param model: Neural network used to generate the prediction
+    :param dataset: Dataset used to test the neural network
+    :param configuration: wandb configuration object. Used to scale the displayed values.
+    :return:
+    """
+    with torch.no_grad():
+        model.eval()
+
+        # Iterate on the pt files
+        files = dataset.dataset.processed_paths
+        for file in files:
+            batch = torch.load(file)
+            file = Path(file)
+
+            y_hat = model.network(batch)
+            display_x_y_y_hat_data(pos=batch.pos.numpy(), x=batch.x.numpy(),
+                                   y=batch.y.numpy(), local_path=file, y_hat=y_hat.numpy(),
+                                   config=configuration)
+
+
+def display_x_y_y_hat_data(pos, x: np.ndarray, y: np.ndarray, local_path: Path, y_hat: np.ndarray = None,
+                           display_features=True, config=None):
     """
     Display the results of the neural network using :
     :param pos: the 3D position of each voxels center
-    :param X: The features of each voxels
-    :param Y: The target of each voxels
-    :param Y_hat: The network output
+    :param x: The features of each voxels
+    :param y: The target of each voxels
+    :param local_path: Path to the loaded file, used to save the output vtk in the same folder.
+    :param y_hat: The network output
     :param display_features: If the input features must be displayed
     :param config: Values scaling configuration (Note, features are not scalled)
     :return:
     """
-    ps.init()
 
     if config is None:
         scaling_temperature = 1
         scaling_size = 1
+        scaling_speed = 1
+        scaling_power = 1
+        scaling_size = 1
     else:
-        scaling_temperature = float(config["scaling_temperature"])
-        scaling_size = float(config["scaling_size"])
+        scaling_temperature = config["scaling_temperature"]
+        scaling_size = config["scaling_size"]
+        scaling_speed = config["scaling_speed"]
+        scaling_power = config["scaling_power"]
+        scaling_size = config["scaling_size"]
 
-    cloud_point = ps.register_point_cloud("my points", pos * scaling_size)
+    cloud_point = pv.PolyData(pos * scaling_size)
 
     if display_features:
-        features_list = ["laser", "speed", "laser power", "layer thickness", "time step duration", "x_type1", "x_type2",
-                         "x_type3", "scaled past temperature", "x past displacement", "y past displacement",
-                         "z past displacement"]
+        features_list = ["laser speed", "laser power", "layer thickness", "time step duration", "baseplate", "part",
+                         "supports", "scaled past temperature"]
+        #"x past displacement", "y past displacement", "z past displacement"
         for i, name in enumerate(features_list):
-            cloud_point.add_scalar_quantity(name, X[:, i])
+            value = x[:, i]
+            if name == "laser power":
+                value *= scaling_power
+            elif name == "laser speed":
+                value *= scaling_speed
+            elif name == "scaled past temperature":
+                value *= scaling_temperature
+
+            cloud_point.point_data["Feature " + name] = value
+
+        # Generate the past displacement vector
+        value = x[:, -4:-1] # " past displacement, y past displacement, z past displacement
+        value *= scaling_size
+        cloud_point.point_data["Feature past displacement (mm)"] = value
 
     # Add target
-    cloud_point.add_scalar_quantity("Target Temperature", Y[:, 0] * scaling_temperature,
-                                    vminmax=(0, scaling_temperature))
-    cloud_point.add_vector_quantity("Target displacement", Y[:, 1:-1] * scaling_size)
+    cloud_point.point_data["Target temperature (C)"] = y[:, 0] * scaling_temperature
+    cloud_point.point_data["Target displacement (mm)"] = y[:, 1:-1] * scaling_size
 
-    if Y_hat is not None:
+    if y_hat is not None:
         # Add prediction
-        cloud_point.add_scalar_quantity("Predicted Temperature", Y_hat[:, 0] * scaling_temperature,
-                                        vminmax=(0, scaling_temperature))
-        cloud_point.add_vector_quantity("Predicted displacement", Y_hat[:, 1:-1] * scaling_size)
+        cloud_point.point_data["Predicted temperature (C)"] = y_hat[:, 0] * scaling_temperature
+        cloud_point.point_data["Predicted displacement (mm)"] = y_hat[:, 1:-1] * scaling_size
 
-    ps.show()
+        # Add error
+        cloud_point.point_data["Error displacement (y - y_hat) (mm)"] = (y[:, 1:-1] - y_hat[:, 1:-1]) * scaling_size
+        cloud_point.point_data["Error temperature (y - y_hat) (C)"] = (y[:, 0] - y_hat[:, 0]) * scaling_temperature
+
+    cloud_point.save(local_path.with_suffix(".vtk"))
