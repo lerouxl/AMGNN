84915c5b5ee4a62f089b770ee0abd8b62d50bb0b
Leopold <lerouxl@cardiff.ac.uk>
2022-05-06 15:54:25 +0100

    Create function to visualise the tmp files generated by the dataloader

---
+++ utils/visualise_pkl.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import numpy as np
+
+from utils.save_arc import load_arc
+from tqdm import tqdm
+from Simufact_ARC_reader.ARC_CSV import Arc_reader
+from utils.visualise import display_x_y_y_hat_data
+
+
+def load_pkl(file_path: Path) -> Arc_reader:
+    """
+    Load a pkl arc file.
+    """
+    arc = load_arc(file_path)
+    return arc
+
+
+if __name__ == "__main__":
+    data_path = Path(r"data\test\tmp_arc")
+    files = list(data_path.glob("*.pkl"))
+    for file in tqdm(files):
+        arc = load_pkl(file)
+        y = np.stack([arc.data.TEMPTURE, arc.data.XDIS, arc.data.YDIS, arc.data.ZDIS]).transpose()
+
+        display_x_y_y_hat_data(pos=arc.coordinate, x=np.zeros(y.shape), y=y, local_path=file,
+                               y_hat=None, display_features=False, config=None)
