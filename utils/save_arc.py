d732f19a6bc935ecffd71b55acb248a2fd38798f
Léopold <36997474+lerouxl@users.noreply.github.com>
2022-10-25 12:51:51 +0100

    Save arc file as a numpy array

--- utils/save_arc.py
+++ utils/save_arc.py
@@ -3,24 +3,79 @@ from simufact_arc_reader.ARC_CSV import Arc_reader
 from pathlib import Path
 from typing import Union
 import logging
+import numpy as np
 
 
 def save_arc(arc: Arc_reader, root_folder: Union[Path, str], name: str):
-    """
+    """Save the useful features of a the arc object,
 
-    :param arc:
-    :param root_folder:
-    :param name:
-    :return:
+    Save the variable of an Arc_reader file so they can be loader in a next step.
+    The file are saved as numpy uncompressed array.
+    Parameters
+    ----------
+    arc: Arc_reader
+        The arc file to save.
+    root_folder:  Union[Path, str]
+        The path to the folder to save the file.
+    name:
+        Name of the file (without extension)
+
+    Returns
+    -------
+    None
     """
     root_folder = Path(root_folder)
     # if folder do not exist, create it
     root_folder.mkdir(parents=True, exist_ok=True)
-    file_path = Path(root_folder) / f"{name}.pkl"
+    file_path = Path(root_folder) / f"{name}.npz"
+
+    # Extract connectivity
+    connectivity = arc.connectivity.astype(int)
+    # Extract edge_index
+    edge_index = np.asarray(arc.edge_index)
+    # Extract coordinate
+    coordinate = arc.coordinate
+    # Extract data
+    temperature = arc.data.TEMPTURE
+    xdis = arc.data.XDIS
+    ydis = arc.data.YDIS
+    zdis = arc.data.ZDIS
+    process_cat = arc.data.process_category
+    process_features = arc.data.process_features
+    # Extract metaparameters
+    initT = arc.metaparameters.initialTemperature_C
+    layerT = arc.metaparameters.layerThickness_m
+    power = arc.metaparameters.power_W
+    step = arc.metaparameters.process_step
+    speed = arc.metaparameters.speed_m_s
+    time_step_lenght = arc.metaparameters.time_steps_length_s
+    time_steps_s = arc.metaparameters.time_steps_s
+
+    # Extract points types
+    points_types = arc.points_types
+
+
 
     # If the arc object was displayed, it cannot be pickled
     with open(file_path, "wb") as file:
-        pickle.dump(arc, file)
+        np.savez(file, connectivity=connectivity,
+                 edge_index=edge_index,
+                 coordinate=coordinate,
+                 temperature=temperature,
+                 xdis=xdis,
+                 ydis=ydis,
+                 zdis=zdis,
+                 process_cat=process_cat,
+                 process_features=process_features,
+                 initT=initT,
+                 layerT=layerT,
+                 power=power,
+                 step=step,
+                 speed=speed,
+                 time_step_lenght=time_step_lenght,
+                 time_steps_s=time_steps_s,
+                 points_types =points_types
+                 )
     if file_path.is_file():
         logging.info(f"Written {file_path} successfully.")
     else:
