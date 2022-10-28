14d1d2f077523b9fe78b230a9758c9d40257ce1e
Léopold <36997474+lerouxl@users.noreply.github.com>
2022-10-28 14:40:56 +0100

    Remove deleted dependency

--- utils/visualise_pkl.py
+++ utils/visualise_pkl.py
@@ -5,7 +5,6 @@ import numpy as np
 from utils.save_arc import load_arc
 from tqdm import tqdm
 from simufact_arc_reader.ARC_CSV import Arc_reader
-from utils.visualise import display_x_y_y_hat_data
 
 
 def load_pkl(file_path: Path) -> Arc_reader:
@@ -23,5 +22,5 @@ if __name__ == "__main__":
         arc = load_pkl(file)
         y = np.stack([arc.data.TEMPTURE, arc.data.XDIS, arc.data.YDIS, arc.data.ZDIS]).transpose()
 
-        display_x_y_y_hat_data(pos=arc.coordinate, x=np.zeros(y.shape), y=y, local_path=file,
-                               y_hat=None, display_features=False, config=None)
+        #display_x_y_y_hat_data(pos=arc.coordinate, x=np.zeros(y.shape), y=y, local_path=file,
+        #                       y_hat=None, display_features=False, config=None)
