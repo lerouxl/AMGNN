ff52359e33a0ffb2cbcff61591b9a7bf4826cfe6
Le Roux <36997474+lerouxl@users.noreply.github.com>
2022-11-21 19:03:53 +0000

    Fix class inheritense

--- model/simple_mlp.py
+++ model/simple_mlp.py
@@ -1,12 +1,15 @@
 from torch import Tensor
 from torch_geometric.nn import MessagePassing
-from torch_geometric.nn import MLP
+#from torch_geometric.nn import MLP
+from model.operations import MLP
 from torch_geometric.data import Data
+import torch
 
 
-class SimpleMlp(MessagePassing):
-    def __int__(self, in_channels: int, hidden_channels: int, out_channels: int, number_hidden: int):
-        """ Simple MLP.
+class SimpleMlp(torch.nn.Module):
+    """"""
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int, number_hidden: int):
+        """Simple MLP.
 
         This neural network is not using the graph and only send all nodes features to an MLP.
 
@@ -21,7 +24,8 @@ class SimpleMlp(MessagePassing):
         number_hidden: int
             Number of hidden layer of the MLP
         """
-        super().__init__(aggr="mean")
+        super(SimpleMlp, self).__init__()
+
 
         self.name = "Simple MLP"
 
@@ -30,10 +34,23 @@ class SimpleMlp(MessagePassing):
         self.out_channels = int(out_channels)
         self.in_channels = int(in_channels)
 
-        self.network = MLP(in_channels=self.in_channels, hidden_channels=self.hidden, act="gelu",
+        self.network = MLP(in_channels=self.in_channels, hidden_channels=self.hidden, act="GELU",
                            out_channels=self.out_channels, num_layers=self.number_hidden)
 
     def forward(self, batch: Data) -> Tensor:
+        """ Apply the neural network to a data batch.
+
+        From a graph, extract just the features of each nodes and make a prediction.
+        Parameters
+        ----------
+        batch: Data
+            Torch geometric graph.
+
+        Returns
+        -------
+        torch.Tensor:
+            Node predictions.
+        """
         # Check that all input features values are bellow 1
         assert (batch.x.max() <= 1)  # If assert is wrong: check data normalisation.
 
