203341a3db6bb2be37643d67a62795e7705cfe21
Leopold <lerouxl@cardiff.ac.uk>
2022-11-22 19:06:10 +0000

    Create a double head version

--- model/simple_gnn.py
+++ model/simple_gnn.py
@@ -2,7 +2,7 @@ from torch import Tensor
 from torch_geometric.nn import MessagePassing
 from model.operations import MLP
 from torch_geometric.data import Data
-
+import torch
 
 class SimpleGnn(MessagePassing):
     """"""
@@ -60,6 +60,72 @@ class SimpleGnn(MessagePassing):
         x = self.propagate(batch.edge_index, x=x)
         x = self.decoder(x)
         return x
+
+    def message(self, x_j):
+        """Compute the message passing by the graph.
+        The message is the features of the neighbors node.
+        """
+        return x_j
+
+class DoubleHeadSimpleGnn(MessagePassing):
+    """"""
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int, number_hidden: int, aggregator: str):
+        """ Simple Graph neural network.
+
+        This neural network is using a encoder decoder architecture with an massage passing them inbetween.
+
+        Parameters
+        ----------
+        in_channels: int
+            Number of features per nodes.
+        hidden_channels: int
+            Size of the hidden layer of the MLP.
+        out_channels: int
+            Number of output features per nodes.
+        number_hidden: int
+            Number of hidden layer of the MLP
+        aggregator: str
+            Type of aggregation function to use for passing message in the graph.
+        """
+        super(DoubleHeadSimpleGnn, self).__init__(aggr=str(aggregator))
+
+        self.name = "Simple GNN"
+
+        self.hidden = int(hidden_channels)
+        self.number_hidden = int(number_hidden)
+        self.out_channels = int(out_channels)
+        self.in_channels = int(in_channels)
+
+        self.encoder = MLP(in_channels=self.in_channels, hidden_channels=self.hidden, act="GELU",
+                           out_channels=self.hidden, num_layers=self.number_hidden)
+        self.decoder1 = MLP(in_channels=self.hidden, hidden_channels=self.hidden, act="GELU",
+                           out_channels=1, num_layers=self.number_hidden)
+        self.decoder2 = MLP(in_channels=self.hidden, hidden_channels=self.hidden, act="GELU",
+                            out_channels=3, num_layers=self.number_hidden)
+
+    def forward(self, batch: Data) -> Tensor:
+        """Apply the neural network to a data batch.
+
+        From a graph, apply the default message passing network with an encoder and decoder.
+        Parameters
+        ----------
+        batch: Data
+            Torch geometric graph.
+
+        Returns
+        -------
+        torch.Tensor:
+            Node predictions.
+        """
+        # Check that all input features values are bellow 1
+        assert (batch.x.max() <= 1)  # If assert is wrong: check data normalisation.
+
+        x = self.encoder(batch.x)
+
+        x = self.propagate(batch.edge_index, x=x)
+        x_1 = self.decoder1(x) # Temperature
+        x_2 = self.decoder2(x) # Displacement
+        return torch.hstack([x_1 , x_2])
 
     def message(self, x_j):
         """Compute the message passing by the graph.
--- model/simple_mlp.py
+++ model/simple_mlp.py
@@ -55,3 +55,58 @@ class SimpleMlp(torch.nn.Module):
         assert (batch.x.max() <= 1)  # If assert is wrong: check data normalisation.
 
         return self.network(batch.x)
+
+
+class DoubleHeadSimpleMlp(torch.nn.Module):
+    """"""
+    def __init__(self, in_channels: int, hidden_channels: int, out_channels: int, number_hidden: int):
+        """Simple MLP.
+
+        This neural network is not using the graph and only send all nodes features to an MLP.
+
+        Parameters
+        ----------
+        in_channels: int
+            Number of features per nodes.
+        hidden_channels: int
+            Size of the hidden layer of the MLP.
+        out_channels: int
+            Number of output features per nodes.
+        number_hidden: int
+            Number of hidden layer of the MLP
+        """
+        super(DoubleHeadSimpleMlp, self).__init__()
+
+
+        self.name = "Simple MLP"
+
+        self.hidden = int(hidden_channels)
+        self.number_hidden = int(number_hidden)
+        self.out_channels = int(out_channels)
+        self.in_channels = int(in_channels)
+
+        self.network1 = MLP(in_channels=self.in_channels, hidden_channels=self.hidden, act="GELU",
+                           out_channels=1, num_layers=self.number_hidden)
+        self.network2 = MLP(in_channels=self.in_channels, hidden_channels=self.hidden, act="GELU",
+                            out_channels=3, num_layers=self.number_hidden)
+
+    def forward(self, batch: Data) -> Tensor:
+        """ Apply the neural network to a data batch.
+
+        From a graph, extract just the features of each nodes and make a prediction.
+        Parameters
+        ----------
+        batch: Data
+            Torch geometric graph.
+
+        Returns
+        -------
+        torch.Tensor:
+            Node predictions.
+        """
+        # Check that all input features values are bellow 1
+        assert (batch.x.max() <= 1)  # If assert is wrong: check data normalisation.
+
+        x_1 = self.network1(batch.x)
+        x_2 = self.network2(batch.x)
+        return torch.hstack([x_1 , x_2])
